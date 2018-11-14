1051978f0206516a0c8b6c20a556a87c8924aa4d
Greg Babiars <gbabiars@gmail.com>
2018-11-14 06:24:56 -0700

    Add basic image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
 import React from "react";
-import "./App.css";
+import Image from "./01/Image";
 
-const App = () => <div />;
+const App = () => {
+  const ids = [
+    "S6L",
+    "SBY",
+    "VPB",
+    "RVS",
+    "VOZ",
+    "VXH",
+    "VXJ",
+    "RD1",
+    "NZT",
+    "RW9",
+    "63B",
+    "VQK",
+    "RW9",
+    "SFZ",
+    "RIA",
+    "SEW"
+  ];
+
+  return (
+    <div>
+      {ids.map(id => (
+        <div>
+          <Image
+            key={id}
+            src={`https://www.chevrolet.com/bypass/seg4_tools/ddp/gmna/assets/US/chevrolet/2019/silverado/silverado-1500-new/large_byo/${id}.jpg`}
+            alt={id}
+          />
+          <div style={{ marginTop: 10, marginBottom: 20 }}>Option: {id}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
